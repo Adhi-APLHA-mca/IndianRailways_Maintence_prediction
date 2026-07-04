2e06e2c9733f10f317c33919bff364aff9f875de
Adhipex69 <dev@mstseverything.com>
2026-07-04 13:08:23 +0530

    implementation of data ingestion pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from dataclasses import dataclass
 import os
 import sys
 from src.logger import logging
+from src.exception import custom_exception
+from sklearn.model_selection import train_test_split
 
 
 @dataclass #decorator used to say this dataclass only saves metadata
@@ -24,9 +26,25 @@ class DataIngestion:
         logging.info("DataIngestion Process has been initialized")
         try:
             df = pd.read_csv(input("please place the path of the csv: "))
-            
-        except:
-            pass
+            logging.info('data loaded sucessfully')
+
+            #to create a artifact folder from self obj path
+            os.makedirs(os.path.join(self.obj.train_dataset_path),exist_ok=True)
+            df.to_csv(self.obj.raw_dataset_path,index=False,header=True)
+
+            train,test = train_test_split(df,test_size=0.3,random_state=42)
+
+            train.to_csv(self.obj.train_dataset_path,index=False,header=True)
+            test.to_csv(self.obj.test_dataset_path,index=False,header=True)
+            logging.info('data ingestion completed for train and test data"')
+
+            return {
+                self.obj.test_dataset_path,
+                self.obj.train_dataset_path
+            }
+        
+        except Exception as e:
+            raise custom_exception(e,sys)
 
 if __name__ == "__main__":
 
